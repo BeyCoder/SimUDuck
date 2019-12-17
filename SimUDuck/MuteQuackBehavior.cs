8fcc11bb8aebb8c855d2f60156d84cb999bb77ae
Beybut Abdulragimov <beybut2013@gmail.com>
2019-12-17 17:19:28 +0400

    Quack functions

---
+++ SimUDuck/MuteQuackBehavior.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimUDuck
+{
+    class MuteQuackBehavior : IQuackBehavior
+    {
+        public void Quack()
+        {
+            Console.WriteLine("Я не умею крякать!");
+        }
+    }
+}
