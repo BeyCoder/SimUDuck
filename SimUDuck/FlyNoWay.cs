f9cb8ddf4b83d5b7b034c07cfd8b1e8e8c327a7a
Beybut Abdulragimov <beybut2013@gmail.com>
2019-12-17 17:10:48 +0400

    Flying functions

---
+++ SimUDuck/FlyNoWay.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimUDuck
+{
+    class FlyNoWay : IFlyBehavior
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Я не умею летать!");
+        }
+    }
+}
---
+++ SimUDuck/FlyWithWings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimUDuck
+{
+    class FlyWithWings : IFlyBehavior
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Я летаю!");
+        }
+    }
+}
